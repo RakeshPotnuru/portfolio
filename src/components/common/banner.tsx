920ca14119ce15ad3effc09626f86d7cb2407e1a
Rakesh Potnuru <rakeshpotnuru321@gmail.com>
2025-12-05 13:23:41 +0530

    Update banner

--- src/components/common/banner.tsx
+++ src/components/common/banner.tsx
@@ -5,7 +5,7 @@ import { siteConfig } from "@/config/site";
 export default function Banner() {
   return (
     <div className="bg-black p-4 text-center">
-      Open for full-time roles.{" "}
+      Urgently looking for full-time roles.{" "}
       <a
         href={siteConfig.links.email}
         className="underline text-blue-500 hover:text-blue-600 hover:underline-offset-2"
